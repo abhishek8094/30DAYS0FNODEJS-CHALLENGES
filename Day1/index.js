a0907cb03b69eef3975dc1703326714fde32eeae
Abhishek Singh <85206759+abhishek8094@users.noreply.github.com>
2024-02-22 08:22:52 +0530

    Add files via upload

---
+++ Day1/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function readFileContent(filePath) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file: ${err.message}`);
+        } else {
+            console.log(`File Content:\n${data}`);
+        }
+    });
+}
+
+readFileContent('test-files/file1.txt');
